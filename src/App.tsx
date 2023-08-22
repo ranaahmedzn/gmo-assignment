819fe3df6abd4cafbba087ae2a1cd7eef8a67e9d
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-08-22 10:53:36 +0600

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Buttons from "./components/Buttons";
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to GMO Assignment!</h3>
+      <Buttons />
+    </div>
+  );
+};
+
+export default App;
